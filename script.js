1f69c97154326ea9614d863e7e5577edd36e2da6
Jason Yoo <sungminyoo@gmail.com>
2022-03-20 18:23:30 -0400

    added finishing touched

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var generateBtn = document.querySelector("#generate");
+
 // Assignment code here
 const passChar = {
   lowercase: 'abcdefghijklmnopqrtsuvwxyz',
@@ -10,36 +12,58 @@ const passChar = {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 function generatePassword() {
   var genPassword = "";
+  var password = "";
 
+  // create prompts for password length
   var length = window.prompt("How long do you want your password to be? (between 8-128 characters)");
+  if (length < 8) {
+    alert("Password must have at least 8 characters!");
+    return "";
+  }
+  if (length > 128) {
+    alert("Password cannot contain more than 128 characters!");
+    return "";
+  }
 
+  // create prompts for lowercase
   var lowercase = window.confirm("Would you like to include lowercase letters?");
   if (lowercase) {
     genPassword += passChar.lowercase;
   };
 
+  // create prompts for uppercase
   var uppercase = window.confirm("Would you like to include uppercase letters?");
   if (uppercase) {
     genPassword += passChar.uppercase;
   };
 
+  // create prompts for numbers
   var numeric = window.confirm("Would you like to include numbers?");
   if (numeric) {
     genPassword += passChar.numeric;
   };
 
+  // create prompts for symbols
   var specialChar = window.confirm("Would you like to include special characters?");
   if (specialChar) {
     genPassword += passChar.specialChar;
   };
 
+  
+  for (let i = 0; i < length; i++) {
+    password += genPassword[Math.floor(Math.random() * genPassword.length)]
+  }
+
+  return password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+

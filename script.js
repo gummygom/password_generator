11cf204821d4d2d247b40c8edd5757ebfc8ee4fb
Jason Yoo <sungminyoo@gmail.com>
2022-03-20 17:55:09 -0400

    added generatePassword function

--- script.js
+++ script.js
@@ -15,7 +15,31 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var genPassword = "";
 
+  var length = window.prompt("How long do you want your password to be? (between 8-128 characters)");
 
+  var lowercase = window.confirm("Would you like to include lowercase letters?");
+  if (lowercase) {
+    genPassword += passChar.lowercase;
+  };
+
+  var uppercase = window.confirm("Would you like to include uppercase letters?");
+  if (uppercase) {
+    genPassword += passChar.uppercase;
+  };
+
+  var numeric = window.confirm("Would you like to include numbers?");
+  if (numeric) {
+    genPassword += passChar.numeric;
+  };
+
+  var specialChar = window.confirm("Would you like to include special characters?");
+  if (specialChar) {
+    genPassword += passChar.specialChar;
+  };
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
